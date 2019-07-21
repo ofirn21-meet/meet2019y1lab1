010b1740be0acb667c0b00039a39b722d884e654
ofirn21-meet <ofirn21@meet.mit.edu>
2019-07-21 16:29:38 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,49 @@
+import turtle 
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+turtle.penup()  
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200) 
+turtle.goto(-200+50,-100) 
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup()#begining of let "e"
+turtle.goto(-200+150,-100)
+turtle.pendown()
+turtle.goto(-200+150,-100+200)
+turtle.goto(-200+250,-100+200)
+turtle.penup()
+turtle.goto(-200+150,-100+100)
+turtle.pendown()
+turtle.goto(-200+250,-100+100)
+turtle.penup()
+turtle.goto(-200+150,-100)
+turtle.pendown()
+turtle.goto(-200+250,-100)
+#end of let "e"
+turtle.penup()
+turtle.goto(-200+300,-100)
+turtle.pendown()
+turtle.goto(-200+300,-100+200)
+turtle.goto(-200+400,-100+200)
+turtle.penup()
+turtle.goto(-200+300,-100+100)
+turtle.pendown()
+turtle.goto(-200+250,-100+100)
+turtle.penup()
+turtle.goto(-200+150,-100)
+turtle.pendown()
+turtle.goto(-200+250,-100)
+
+
+
+# ...and end it before the next line.
+turtle.mainloop() 
+# turtle.mainloop() tells the turtle to do all
+# the turtle commands above it and paint it on the screen.
+# It always has to be the last line of code!
